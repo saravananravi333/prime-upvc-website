363b8b52bf73254ff00b45318c83540be858d7a2
Saravanan Ravi <saravananrs333@gmail.com>
2024-09-06 13:21:22 +0530

    Blogs Section Added 06-09-2024

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// ----- BLOG - READ MORE -----
+function toggleReadMore(button) {
+    const moreText = button.parentElement.querySelector('.more-text');
+
+    if (moreText.style.display === 'inline') {
+        moreText.style.display = 'none';
+        button.innerHTML = 'Read More...';
+    } else {
+        moreText.style.display = 'inline';
+        button.innerHTML = 'Read Less <i class="ri-arrow-up-fill"></i>';
+    }
+}
+
+document.querySelectorAll('.more-text').forEach(function (span) {
+    span.style.display = 'none';
+});
